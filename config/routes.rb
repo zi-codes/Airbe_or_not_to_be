ae19a5bc8fd38a4a4e0c9a9b481bca27727b6a62
Student <ziwei.polaris@gmail.com>
2019-07-30 16:15:18 +0100

    added authentication and login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   get 'listings/show'
   get 'listings/new'
   get 'listings/edit'
+
+  # get '/signup' => 'users#new'
+  # post '/users' => 'users#create'
+
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy' 
+
   resources :listings
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -19,7 +19,7 @@ def signup
   fill_in 'name', with: 'Ian Aquino'
   fill_in 'username', with: 'iamanewuser'
   fill_in 'email', with: 'thisismyemail@yahoo.com'
-  fill_in 'user[phone_number]', with: 'hellooo'
+  fill_in 'user[phone_number]', with: '07923489392'
   fill_in 'password', with: 'password123'
   click_button 'Create User'
 end
