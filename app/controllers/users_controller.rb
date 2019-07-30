e491f1206abdee4df5d800950ba31be8bb4826e7
iAquino47 <ianaquino47@gmail.com>
2019-07-30 14:12:19 +0100

    new commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to users_new_path
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :email, :phone_number, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/delete'
   get 'listings/index'
   get 'listings/show'
   get 'listings/new'
