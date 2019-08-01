7eeaca5f8b3018ba0d604e5ca2c83132f3c08a87
Kaja <catcalledkaja@gmail.com>
2019-08-01 15:09:58 +0100

    adding button

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,6 +7,7 @@ class ListingsController < ApplicationController
 
   def show
     @listing = Listing.find(params[:id])
+    p @bookings = Booking.where("listing_id = #{@listing.id}")
   end
 
   def new
@@ -17,20 +18,20 @@ class ListingsController < ApplicationController
     start_date = params[:listing][:from_date]
     end_date = params[:listing][:to_date]
 
-    p "unparsed start date is #{start_date}"
-    p "unparsed end date is #{end_date}"
+    "unparsed start date is #{start_date}"
+    "unparsed end date is #{end_date}"
 
     parsed_start = Date.parse(start_date)
     parsed_end = Date.parse(end_date)
 
-    p "parsed start date is #{parsed_start}"
-    p "parsed end date is #{parsed_end}"
+    "parsed start date is #{parsed_start}"
+    "parsed end date is #{parsed_end}"
 
     @user = User.find(session[:user_id])
     @listing = @user.listings.create(listing_params)
 
       (parsed_start..parsed_end).each do |date|
-      p @booking = @listing.bookings.create(date: date, availability: true)
+      @booking = @listing.bookings.create(date: date, availability: true)
     end
 
     redirect_to listings_index_path
