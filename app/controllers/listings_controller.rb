368ec993a72e759b9644c26c18ba65894d75840d
Kaja <catcalledkaja@gmail.com>
2019-08-01 13:48:54 +0100

    Adding date database and having individual dates for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -14,17 +14,23 @@ class ListingsController < ApplicationController
   end
 
   def create
-    p start_date = params[:listing][:from_date]
-    p end_date = params[:listing][:from_date]
+    start_date = params[:listing][:from_date]
+    end_date = params[:listing][:to_date]
 
-    p parsed_start = Date.parse(start_date)
-    p parsed_end = Date.parse(end_date)
+    p "unparsed start date is #{start_date}"
+    p "unparsed end date is #{end_date}"
+
+    parsed_start = Date.parse(start_date)
+    parsed_end = Date.parse(end_date)
+
+    p "parsed start date is #{parsed_start}"
+    p "parsed end date is #{parsed_end}"
 
     @user = User.find(session[:user_id])
     @listing = @user.listings.create(listing_params)
 
-    (parsed_start..parsed_end).each do |date|
-      @booking = @listing.bookings.create(date: date, availability: true)
+      (parsed_start..parsed_end).each do |date|
+      p @booking = @listing.bookings.create(date: date, availability: true)
     end
 
     redirect_to listings_index_path
