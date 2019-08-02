fea60ad36e6d8f9321949ba05609e4feb7cb3148
Kaja <catcalledkaja@gmail.com>
2019-08-02 14:16:20 +0100

    adding jQuery

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,4 +1,4 @@
-$(function() {
+document.addEventListener("turbolinks:load", function() {
   $('.listing-expand-button').on('click', function() {
     var target = $(this);
     var listing = target.closest('.listing')
@@ -7,6 +7,16 @@ $(function() {
     var willBeHidden = !isHidden;
     description.prop("hidden", willBeHidden)
   })
-});
+})
+// $(function() {
+//   $('.listing-expand-button').on('click', function() {
+//     var target = $(this);
+//     var listing = target.closest('.listing')
+//     var description = listing.find('.listing-description');
+//     var isHidden = description.prop("hidden");
+//     var willBeHidden = !isHidden;
+//     description.prop("hidden", willBeHidden)
+//   })
+// });
 
 // find is the same as dollar except that you apply to the jQuery elemnt  where you want to start the search
